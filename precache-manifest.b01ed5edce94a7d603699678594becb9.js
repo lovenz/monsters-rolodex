bef12a9e913a863c8d1e5675d7fa9d0ddc4e97c4
chuanbao <mcb.nz.595@gmail.com>
2020-05-21 11:12:30 -0600

    Updates

---
+++ precache-manifest.b01ed5edce94a7d603699678594becb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4e20ed0cb0fb2f2927cf3e53e20c1b",
+    "url": "/lovenz/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "00cf4f95bfa931e3b73a",
+    "url": "/lovenz/monsters-rolodex/static/css/main.0b24478e.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/lovenz/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lovenz/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cf4f95bfa931e3b73a",
+    "url": "/lovenz/monsters-rolodex/static/js/main.2960f91b.chunk.js"
+  },
+  {
+    "revision": "aeff7ae10f41194a55c0",
+    "url": "/lovenz/monsters-rolodex/static/js/runtime-main.31820283.js"
+  }
+]);
